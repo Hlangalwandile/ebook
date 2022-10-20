133ed2baac57421e27b8e00fe13421acbc2b5e19
Hlanga <82909736+Hlangalwandile@users.noreply.github.com>
2022-10-20 10:00:45 +0200

    ebook page

---
+++ dlonra_library/database/migrations/2022_10_17_125626_create_units_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUnitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('units', function (Blueprint $table) {
+            $table->id();
+            $table->integer('moduleID');
+            $table->integer('order');
+            $table->integer('type');
+            $table->string('header');
+            $table->text('text');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('units');
+    }
+}
---
+++ dlonra_library/resources/views/ebook/page.blade.php
@@ -0,0 +1,78 @@
+@extends('layouts.ebook')
+@section('content')
+<nav class="navbar navbar-expand d-flex flex-column align-item-start" id="ebook-sidebar">
+    <a href="#" class="navbar-brand text-light mt-5">
+        <div class="display-5 font-weight-bold">
+            <span class="sidebarNavigator">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" stroke="#969696" stroke-width="2" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+                  </svg>
+                <span class="chapter">chapter</span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" stroke="#969696" stroke-width="2" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+                  </svg>
+            </span>
+        </div>
+    </a>
+    <ul class="navbar-nav d-flex flex-column mt-5 w-100">
+        <li class="nav-item w-100">
+            <a href="#" class="nav-link text-light pl-4">Home</a>
+        </li>
+        <li class="nav-item w-100">
+            <a href="#" class="nav-link text-light pl-4">Home</a>
+        </li>
+        <li class="nav-item w-100">
+            <a href="#" class="nav-link text-light pl-4">Home</a>
+        </li>
+        <li class="nav-item w-100">
+            <a href="#" class="nav-link text-light pl-4">Home</a>
+        </li>
+    </ul>
+</nav>
+<section class="p-4 module">
+    <button class="btn md-4" id="menu-btn">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 1 16 16">
+            <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+          </svg>
+    </button>
+    <h1><span class="ModuleOrderNumber">01</span><br> Atomic Theory</h1>
+    <div class="unit">
+        <h3>Outcomes <br> After you have completed this chapter you should be able to:</h3>
+        <ul>
+            <li>Understand the composition of matter</li>
+        </ul>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi cumque ex quibusdam molestiae voluptates officia quisquam, quidem consequuntur maxime placeat non animi rem laudantium voluptatibus culpa quam soluta neque fuga?
+        Nemo, ab. Quaerat vel laborum molestiae cum amet, corrupti ad recusandae. Perspiciatis quia numquam sed cupiditate quod obcaecati atque, necessitatibus nesciunt dolorum beatae perferendis vero sunt temporibus dolor nisi sequi!
+        Eos aperiam corrupti libero nesciunt eum autem perspiciatis sunt odio! Ratione laboriosam aliquid officia fugiat nisi totam beatae repellat iusto consequuntur, magnam maxime quo non iste numquam accusamus commodi! Assumenda!
+        Ut ad fuga modi, mollitia consectetur voluptas minus dolore incidunt commodi facilis necessitatibus nobis rerum impedit quam laboriosam soluta. Ipsa blanditiis dolorem similique illum porro excepturi optio voluptates doloremque modi.
+        Itaque minus eveniet, aut facere quasi laudantium necessitatibus, recusandae amet blanditiis voluptate ducimus quis neque expedita temporibus adipisci repudiandae quaerat, officiis inventore voluptatibus iste! Id illum voluptatibus illo doloremque molestiae?</p>
+    </div>
+    <div class="unit">
+        <h3>Outcomes <br> After you have completed this chapter you should be able to:</h3>
+        <ul>
+            <li>Understand the composition of matter</li>
+        </ul>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi cumque ex quibusdam molestiae voluptates officia quisquam, quidem consequuntur maxime placeat non animi rem laudantium voluptatibus culpa quam soluta neque fuga?
+        Nemo, ab. Quaerat vel laborum molestiae cum amet, corrupti ad recusandae. Perspiciatis quia numquam sed cupiditate quod obcaecati atque, necessitatibus nesciunt dolorum beatae perferendis vero sunt temporibus dolor nisi sequi!
+        Eos aperiam corrupti libero nesciunt eum autem perspiciatis sunt odio! Ratione laboriosam aliquid officia fugiat nisi totam beatae repellat iusto consequuntur, magnam maxime quo non iste numquam accusamus commodi! Assumenda!
+        Ut ad fuga modi, mollitia consectetur voluptas minus dolore incidunt commodi facilis necessitatibus nobis rerum impedit quam laboriosam soluta. Ipsa blanditiis dolorem similique illum porro excepturi optio voluptates doloremque modi.
+        Itaque minus eveniet, aut facere quasi laudantium necessitatibus, recusandae amet blanditiis voluptate ducimus quis neque expedita temporibus adipisci repudiandae quaerat, officiis inventore voluptatibus iste! Id illum voluptatibus illo doloremque molestiae?</p>
+    </div>
+    <div class="unit">
+        <h3>Outcomes <br> After you have completed this chapter you should be able to:</h3>
+        <ul>
+            <li>Understand the composition of matter</li>
+        </ul>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi cumque ex quibusdam molestiae voluptates officia quisquam, quidem consequuntur maxime placeat non animi rem laudantium voluptatibus culpa quam soluta neque fuga?
+        Nemo, ab. Quaerat vel laborum molestiae cum amet, corrupti ad recusandae. Perspiciatis quia numquam sed cupiditate quod obcaecati atque, necessitatibus nesciunt dolorum beatae perferendis vero sunt temporibus dolor nisi sequi!
+        Eos aperiam corrupti libero nesciunt eum autem perspiciatis sunt odio! Ratione laboriosam aliquid officia fugiat nisi totam beatae repellat iusto consequuntur, magnam maxime quo non iste numquam accusamus commodi! Assumenda!
+        Ut ad fuga modi, mollitia consectetur voluptas minus dolore incidunt commodi facilis necessitatibus nobis rerum impedit quam laboriosam soluta. Ipsa blanditiis dolorem similique illum porro excepturi optio voluptates doloremque modi.
+        Itaque minus eveniet, aut facere quasi laudantium necessitatibus, recusandae amet blanditiis voluptate ducimus quis neque expedita temporibus adipisci repudiandae quaerat, officiis inventore voluptatibus iste! Id illum voluptatibus illo doloremque molestiae?</p>
+    </div>
+</section>
+
+
+
+
+
+@endsection
